ec81f6ad5d49598717c106124767bc8d477c23dd
TCCINS <TCCINS@DESKTOP-Q0KA7I0>
2022-08-12 16:06:58 +0900

    템플릿 프로젝트 커밋

---
+++ TCCINS/src/main/java/com/tccins/template/TccinsApplication.java
@@ -0,0 +1,13 @@
+package com.tccins.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TccinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TccinsApplication.class, args);
+	}
+
+}
