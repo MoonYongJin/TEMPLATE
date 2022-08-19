e4464de20c11c350d64054d9d38c6ea67d5a4d5f
TCCINS <TCCINS@DESKTOP-Q0KA7I0>
2022-08-19 16:38:17 +0900

    템플릿

--- TCCINS/src/main/java/com/tccins/template/TccinsApplication.java
+++ TCCINS/src/main/java/com/tccins/template/TccinsApplication.java
@@ -2,6 +2,9 @@ package com.tccins.template;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.tccins.template.Login.SHA512PasswordEncoder;
 
 @SpringBootApplication
 public class TccinsApplication {
@@ -9,5 +12,4 @@ public class TccinsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TccinsApplication.class, args);
 	}
-
 }
